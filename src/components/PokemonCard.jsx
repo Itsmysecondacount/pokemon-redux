53d8380d88728d0b6e692c83a63b1389dd2b5880
Jorge Castillo <jorge.castillo.torres.a@gmail.com>
2023-04-09 20:02:24 -0500

    [ADD] react thunk

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -3,16 +3,13 @@ import { Card } from 'antd';
 import Meta from 'antd/lib/card/Meta';
 import './PokemonList.css';
 
-const PokemonCard = ({ name, image }) => {
-  return (
-    <Card
-      title={name}
-      cover={<img src={image} alt={name} />}
-      extra={<StarOutlined />}
-    >
-      <Meta description='fire, magic' />
-    </Card>
-  );
+const PokemonCard = ({ name, image, abilities }) => {
+	const abilitiesUnion = abilities.map((elem) => elem.ability.name).join(', ');
+	return (
+		<Card title={name} cover={<img src={image} alt={name} />} extra={<StarOutlined />}>
+			<Meta description={abilitiesUnion} />
+		</Card>
+	);
 };
 
 export default PokemonCard;
